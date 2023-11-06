b972ab5d1d16e6b6fd2d538231d4aa604c2103e1
Crowdbotics <team@crowdbotics.com>
2023-11-06 06:30:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "registrationform-44360",
+  url: "https://registrationform-44360.botics.co",
+  api: "https://registrationform-44360.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
